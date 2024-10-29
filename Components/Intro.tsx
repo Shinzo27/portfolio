32613ac8b13e330bd1f0e7af9495afed04082b20
Shinzo27 <20bmiit031@gmail.com>
2024-10-30 01:40:09 +0530

    Changes

--- Components/Intro.tsx
+++ Components/Intro.tsx
@@ -3,17 +3,32 @@
 import Image from "next/image";
 
 const Intro = () => {
-    return (
-        <div className="w-full flex flex-col-reverse lg:flex-row gap-14 justify-between items-center -z-10 font-sequel">
-            <div className="flex flex-col gap-2 w-full lg:w-4/5">
-                <h1 className="text-4xl font-bold mb-4">Hi, I'm Pratham.</h1>
-                <p className="text-base text-white text-justify">I'm a full stack developer, and I love to build things. I'm currently working on a portfolio website. I'm also a big fan of music and I love to play guitar. I'm also a big fan of movies and I love to watch them. I'm also a big fan of music and I love to play guitar.</p>
-            </div>
-            <div className="flex items-center justify-center md:mb-0">
-                <Image src="/profile.jpg" alt="Me" width={160} height={160} className="border-2 border-gray-100 rounded-full object-cover" />
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="w-full flex flex-col-reverse lg:flex-row gap-14 justify-between items-center -z-10 font-sequel">
+      <div className="flex flex-col gap-2 w-full lg:w-4/5">
+        <h1 className="text-4xl font-bold mb-4">Hi, I&apos;m Pratham.</h1>
+        <p className="text-base text-white text-justify">
+          Motivated and detail-oriented full stack developer with hands-on
+          experience in building and deploying scalable web applications. Proven
+          ability to develop both frontend and backend solutions, manage
+          databases, and implement real-time features. Experienced in leveraging
+          modern technologies and tools to create seamless user experiences and
+          optimize performance. Eager to contribute to innovative projects and
+          collaborate in a dynamic team environment, with a focus on continuous
+          learning and problem-solving.
+        </p>
+      </div>
+      <div className="flex items-center justify-center md:mb-0">
+        <Image
+          src="/profile.jpg"
+          alt="Me"
+          width={160}
+          height={160}
+          className="border-2 border-gray-100 rounded-full object-cover"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Education from "@/Components/Education";
-import Footer from "@/Components/Footer";
 import GetInTouch from "@/Components/GetInTouch";
 import Intro from "@/Components/Intro";
 import Projects from "@/Components/Projects";
@@ -7,7 +6,6 @@ import Skills from "@/Components/Skills";
 import Meteors from "@/Components/ui/meteors";
 import { Spotlight } from "@/Components/ui/spotlight";
 import WorkExperience from "@/Components/WorkExperience";
-import Image from "next/image";
 
 export default function Home() {
   return (
