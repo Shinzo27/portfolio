63a2f32ebf7b949df8fb4242f1b851c8e71174e9
Shinzo27 <20bmiit031@gmail.com>
2024-11-20 11:17:41 +0530

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Footer from "@/Components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -34,6 +35,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased text-white bg-neutral-950`}
       >
         {children}
+        <Analytics />
         <Footer/>
       </body>
     </html>
